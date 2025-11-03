d640acba4421c8c299a07d38063485c01379a8a0
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-11-03 15:49:57 +0900

    async #finish

--- javascript-practices/async/async-fns.js
+++ javascript-practices/async/async-fns.js
@@ -2,9 +2,11 @@
 exports.asyncFn01 = function(param, callback) {
     // 비동기 코드: 파일 시스템(IO), 네트워크 통신(fetch), SQL to DB, setTimeout, ... etc
     setTimeout(() => {
+        
         //
-        // 비동기 처리~~
+        // 비동기 처리
         //
+
         if(param !== null) {
             callback(null, 'sucess');
         } else {
@@ -14,8 +16,26 @@ exports.asyncFn01 = function(param, callback) {
     }, 3000);
 }
 
-// 2. Promise
-// 3. async ~ await
+// 2. Promise 지원 비동기 함수
+exports.asyncFn02 = function(param) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            
+            //
+            // 비동기 처리
+            //
+            
+            if(param !== null) {
+                // 성공
+                resolve('sucess');
+            } else {
+                // 실패
+                reject(new Error('fail'));
+            }    
+
+        }, 3000);
+    });
+}
 
 
 
